11d9937b99b6242fcb5ae6685d4d0e9e4119e25a
Amaro Amorim <oamaro0410@hotmail.com>
2022-08-02 20:09:22 -0300

    FlarList

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 
 import Topo from './Cesta/componentes/Topo';
 import Detalhes from './Cesta/componentes/Detalhes';
-import Itens from './Cesta/componentes/Itens';
+import Item from './Cesta/componentes/Item';
 
 
 
 
 export default function Cesta({ topo, detalhes, itens }) {
-    return <ScrollView>
-    <Topo {...topo} />
-    
-    <View style={estilos.cesta}>
-       <Detalhes {...detalhes} />
-       <Itens {...itens} /> 
-    </View>
-    </ScrollView>
+    return <>
+        <FlatList
+            data={itens.lista}
+            renderItem={Item}
+            keyExtractor={({ nome }) => nome}
+            ListHeaderComponent={() => {
+                return <>
+                    <Topo {...topo} />
+                    <View style={estilos.cesta}>
+                        <Detalhes {...detalhes} />
+                    </View>
+                </>
+            }}
+        />
+    </>
 }
 
 const estilos = StyleSheet.create({
+    titulo: {
+        color: "#464646",
+        fontWeight: "bold",
+        marginTop: 32,
+        marginBottom: 8,
+        fontSize: 20,
+        lineHeight: 32,
+    },
     cesta: {
         paddingVertical: 8,
         paddingHorizontal: 16
